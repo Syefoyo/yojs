550a20c5089d7ce6ec456e16846cae28291c666e
Syefoyo <trisyefolionel@gmail.com>
2024-05-16 09:28:20 +0700

    yojs

---
+++ yo.js
@@ -0,0 +1,6 @@
+function doNow() {
+    document.getElementById("now")
+    .innerHTML = "Hi,Im The Owner Of Morning Inc,first i started this Game Making Group I get It By Roblox,And I Dosent Want To Do As Just A PLAYER,So i Learn Some Coding And I achieved How to Web Development (A bit) im studying JavaScript Still And yeah Our Upcoming Game Will Be Show In the NEXT Page And Make Sure To read My crew blog. "
+}
+let name = window.prompt("Your Name again Please :)")
+document.write("Hello again" + " " + name)
